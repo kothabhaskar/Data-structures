3415d8a6750a3b5a7ad528ab2d9ee077e7ba9b7b
kothabhaskar <kothabhaskar@gmail.com>
2020-08-07 22:13:17 +0530

    Update pair_equal_sum.cpp

--- pair_equal_sum.cpp
+++ pair_equal_sum.cpp
@@ -33,14 +33,39 @@ void find_pair_element_equl_sum(vector<int>arr, int sum)
     }
    
 }
+void find_pair_element_equl_sum_sortedArray(vector<int>arr, int sum)
+{
+    
+    for(int i = 0, j = arr.size()-1; i <j;)
+    {
+        if(arr[i]+arr[j] ==  sum )
+        {
+            cout<<"sorted Array Pair val "<<arr[i]<<" and "<<arr[j]<<endl;
+            j--; i++;
+        }
+        else if(arr[j] + arr[i] >  sum)
+        {
+            j--;
+        }
+        else
+        {
+            i++;
+        }
+       
+    }
+   
+}
 
 int main()
 {
     cout<<"Hello World\n";
     
-    vector<int>  arraMisMoreNum = {1,6,5,4,8,2,9,3,7,10};// duplicate more then one element but  not start from 1
+    vector<int>  arraMisMoreNum = {1,6,5,4,8,2,9,3,7,10};// find pair 
     find_pair_element_equl_sum_hash(arraMisMoreNum, 12, 10);
     cout<<"\n----\n";
     find_pair_element_equl_sum(arraMisMoreNum, 12);
+    cout<<"\n----\n";
+     vector<int>  arrSortNum = {1,2,3,4,5,6,7,8,9,10};// find pair 
+    find_pair_element_equl_sum_sortedArray(arrSortNum, 12);
     return 0;
 }
